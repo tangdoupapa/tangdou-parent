5ebd2c5e2b0cbd652170b07afd176992eccd0ae8
felix <fanyaoxuan@outlook.com>
2020-04-24 18:39:06 +0800

    1、hystrix 实时监控组件

---
+++ tangdou-spring-cloud-study/tangdou-spring-cloud-hystrix-dashboard-9001/src/main/java/com/tangdou/spring/cloud/HystrixDashboard9001.java
@@ -0,0 +1,16 @@
+package com.tangdou.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableHystrixDashboard
+public class HystrixDashboard9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard9001.class, args);
+    }
+}
---
+++ tangdou-spring-cloud-study/tangdou-spring-cloud-hystrix-dashboard-9001/src/main/java/com/tangdou/spring/cloud/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package com.tangdou.spring.cloud.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * (Payment)表控制层
+ *
+ * @author makejava
+ * @since 2020-03-06 14:22:26
+ */
+@Api
+@RestController
+@RequestMapping("payment")
+public class PaymentController {
+
+}
