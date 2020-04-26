30c766f8f4fe444944c1338cd844d002ba3c77a7
felix <fanyaoxuan@outlook.com>
2020-04-26 10:12:52 +0800

    1、hystrix dashboard 实时监控

--- tangdou-spring-cloud-study/tangdou-spring-cloud-hystrix-provider-8001/src/main/java/com/tangdou/spring/cloud/HystrixProvider8001.java
+++ tangdou-spring-cloud-study/tangdou-spring-cloud-hystrix-provider-8001/src/main/java/com/tangdou/spring/cloud/HystrixProvider8001.java
@@ -1,11 +1,14 @@
 package com.tangdou.spring.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -20,5 +23,23 @@ public class HystrixProvider8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixProvider8001.class, args);
+    }
+
+
+    /**
+     * 此配置是为了服务监控而配置，与服务容错本身无观，springCloud 升级之后的坑
+     * ServletRegistrationBean因为springboot的默认路径不是/hystrix.stream
+     * 只要在自己的项目中配置上下面的servlet即可
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
