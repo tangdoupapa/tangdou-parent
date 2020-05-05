4ecd043531dd07b7ee9d2867819819a5d6437fd3
tangdoupapa <fanyaoxuan@outlook.com>
2020-05-05 08:38:27 +0800

    1、config 配置完成

--- tangdou-spring-cloud-study/tangdou-spring-cloud-config-customer-3355/src/main/java/com/tangdou/spring/cloud/customer/ConfigCustomerBootstrap3355.java
+++ tangdou-spring-cloud-study/tangdou-spring-cloud-config-customer-3355/src/main/java/com/tangdou/spring/cloud/customer/ConfigCustomerBootstrap3355.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableSwagger2
+@EnableEurekaClient
 public class ConfigCustomerBootstrap3355 {
 
     public static void main(String[] args) {
