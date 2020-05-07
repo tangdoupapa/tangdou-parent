9ebb5bd0008506a4fa345a58209a0f885b107727
felix <fanyaoxuan@outlook.com>
2020-05-07 18:52:50 +0800

    1、alibaba sentinel 测试持久化等(不含熔断  ribbon feign 与之前无差别)

--- tangdou-spring-cloud-study/tangdou-spring-cloud-nacos-customer-83/src/main/java/com/tangdou/spring/cloud/customer/controller/OrderController.java
+++ tangdou-spring-cloud-study/tangdou-spring-cloud-nacos-customer-83/src/main/java/com/tangdou/spring/cloud/customer/controller/OrderController.java
@@ -1,13 +1,13 @@
 package com.tangdou.spring.cloud.customer.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.tangdou.common.base.entity.Payment;
 import com.tangdou.common.base.result.DataResult;
 import com.tangdou.common.base.result.Result;
+import com.tangdou.common.base.util.ResultUtil;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -19,8 +19,8 @@ public class OrderController {
 
 //    public static final String PAYENT_URL = "http://localhost:8001";
 
-//        public static final String PAYENT_URL = "http://tangdou-spring-cloud-nacos-provider";
-        public static final String PAYENT_URL = "http://tangdou-spring-cloud-nacos-config";
+    //        public static final String PAYENT_URL = "http://tangdou-spring-cloud-nacos-provider";
+    public static final String PAYENT_URL = "http://tangdou-spring-cloud-nacos-config";
     @Resource
     private RestTemplate restTemplate;
 
@@ -34,5 +34,23 @@ public class OrderController {
     public Result<Payment> getPayment(@PathVariable("id") long id) {
         return restTemplate.getForObject(PAYENT_URL + "/payment/get/" + id, DataResult.class);
     }
+
+    @GetMapping("payment/select/{id}")
+    public Result<Payment> getPaymentOther(@PathVariable("id") long id) {
+//        int a= 10/0;
+        return restTemplate.getForObject(PAYENT_URL + "/payment/get/" + id, DataResult.class);
+    }
+
+    @GetMapping("payment/select")
+    @SentinelResource(value = "comsumerPaymentSelect",blockHandler = "blockHandler")
+    public Result<Payment> getPaymentParam(@RequestParam(value = "id", required = false) long id) {
+//        int a= 10/0;
+        return restTemplate.getForObject(PAYENT_URL + "/payment/get/" + id, DataResult.class);
+    }
+
+    public Result<Payment> blockHandler(BlockException e){
+        return ResultUtil.successData(null,"热点规则出发");
+    }
+
 
 }
